23e62c4ecc3287193421b3732ea5952ec78604cb
Luma Rodrigues <lumasilvarn@gmail.com>
2026-02-20 00:00:31 -0300

    Add files via upload

---
+++ diagnostico/diagnostico.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+// CASO A: Escalabilidade. Devido ao alto fluxo de acessos, houve uma precarização no sistema. Com isso, entende-se que não foi bem idealizado para comportar o fluxo massivo de dados.
+
+//CASO B: Confiabilidade. Por sua vez, nesse caso houve uma quebra da confidencialidade de dados do cliente, o que acarreta em uma exposição indesejada e complexa.
+
+//CASO C: Manutenibilidade. Dessa vez, a falta organizacional de desenvolvedores anteriores, trouxe consigo uma dificuldade na continuação e aprimoramento do código, pois este pilar certifica que a organização evidente simplifica a continuidade. 
+
+func main()  {
+	fmt.Println ("Ambiente configurado por Luma Rodrigues")
+	fmt.Escanear ()
+
+}
